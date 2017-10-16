411812e54adec036ada27ca07eb1ff28d7d9a5c2
josefdez96 <32834438+josefdez96@users.noreply.github.com>
2017-10-16 10:53:51 +0200

    Add files via upload

---
+++ Animal.java.java
@@ -0,0 +1,23 @@
+
+public class Animal{
+	private String nombre;
+	private int edad;
+
+
+
+	public void nace(){
+            System.out.print ("Hola, he nacido. ");
+}
+    public String getNombre (){
+	        return nombre;
+}
+    public int getEdad(){
+	        return nombre;
+}
+    public void setNombre(String nombre){
+	        this .nombre = nombre;
+}
+    public void setEdad(int edad){
+	        this.edad = edad;
+    }
+}
